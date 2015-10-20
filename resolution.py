ac3d1965778f9469d2eb9a4f335c25703d72e6a0
Ethan Welsh <ejw45@pitt.edu>
2015-10-20 17:18:35 -0400

    rename files

--- resolution.py
+++ resolution.py
@@ -3,85 +3,58 @@ import itertools
 
 
 class Sentence:
-    def __init__(self, sentence):
-        self.terms = list()
-        for term in sentence:
-
-            if isinstance(term, tuple):
-                self.terms.append("~{}".format(term[1]))
-            else:
-                self.terms += [term]
-
-    def remove(self, item):
-        self.terms.remove(item)
-
-    #def __str__(self):
-    #    return str(str(self.terms))
-
-    def __contains__(self, item):
-        return item in self.terms
+    def __init__(self, clauses):
+        self.clauses = {c for c in clauses}
 
     def __eq__(self, other):
-
-        if isinstance(other, Sentence):
-            return self.terms == other.terms
-        if isinstance(other, str):
-            return self.terms[0] == other
+        return other.clauses == self.clauses
 
     def __repr__(self):
-        return str(str(self.terms))
+        return str((str(self.clauses)))
 
-    __hash__ = lambda self: hash(repr(self))
+    def __hash__(self):
+        return hash(repr(self))
+
+    def __ror__(self, other):
+        return self.clauses | other
 
 
-def negate(sentence):
+def negate(x):
+    assert(isinstance(x, str))
 
-    if sentence[0] == '~':
-        return sentence[1:]
+    if x[0] == '~':
+        return x[1:]
     else:
-        return '~' + sentence
+        return "~" + x
 
 
 def resolve(sen_a, sen_b):
 
-    if isinstance(sen_a, Sentence):
-        sen_a = sen_a.terms
-    if isinstance(sen_b, Sentence):
-        sen_b = sen_b.terms
-    if isinstance(sen_a, str):
-        sen_a = [sen_a]
-    if isinstance(sen_b, str):
-        sen_b = [sen_b]
+    resolvents = set()
 
-    literals = sen_a + sen_b
-    resolvents = []
-
-    resolution_occured = False
-
-    for (x, y) in itertools.combinations(literals, 2):
+    for (x, y) in itertools.product(list(sen_a.clauses), list(sen_b.clauses)):
         if x == negate(y):
-            resolution_occured = True
-            resolvents += (set(literals) - {x, negate(x)})
+            step = list((sen_a.clauses - {x}) | (sen_b.clauses - {y}))
 
-    if resolution_occured:
-        return resolvents
-    else:
-        return list(set(literals))
+            if not step:
+                resolvents |= {None}
+            else:
+                resolvents |= {Sentence(step)}
+
+    return resolvents
 
 
 def resolution(kb, alpha):
-
-    clauses = kb | {Sentence((('not', alpha),('not', alpha)))}
+    clauses = kb | {Sentence([negate(alpha)])}
     new = set()
+
     while True:
         for clause_a, clause_b in itertools.combinations(clauses, 2):
-
             resolvents = resolve(clause_a, clause_b)
-            #print(clause_a, clause_b, resolvents)
 
-            if [] in resolvents:
-                return True
-            new |= set(resolvents)
+            if None in resolvents:
+                return False
+            new |= resolvents
         if clauses.issuperset(new):
             return [str(clause) for clause in list(set(clauses))]
         clauses |= new
@@ -91,14 +64,22 @@ def main():
 
     cnf = ast.literal_eval("[[('not', 'Mythical'), ('not', 'Mortal')], ['Mythical', 'Mortal'], ['Mythical', 'Mammal'], ['Mortal', 'Horned'], [('not', 'Mammal'), 'Horned'], [('not', 'Horned'), 'Magical']]")
 
-    knowledge_base = set()
+    kb = set()
 
-    for sen in cnf:
-        knowledge_base |= {Sentence(sen)}
+    for clause in cnf:
+        add = []
 
-    print(resolution(knowledge_base, 'Horned'))
-    print(resolution(knowledge_base, 'Magical'))
-    print(resolution(knowledge_base, 'Mythical'))
+        for sen in clause:
+            if isinstance(sen, tuple):
+                add.append(negate(sen[1]))
+            else:
+                add.append(sen)
+        kb |= {Sentence(add)}
+
+    print(resolution(kb, 'Horned'))
+    print(resolution(kb, 'Magical'))
+    print(resolution(kb, 'Mythical'))
+
 
 if __name__ == '__main__':
     main()
